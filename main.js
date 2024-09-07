db7f870c45b5eddbf06af8b3c77d8e79e073660f
jovanlekic12 <lekicjovan1212@gmail.com>
2024-09-07 23:23:43 +0200

    refactor code

--- main.js
+++ main.js
@@ -4,3 +4,38 @@ window.addEventListener("load", function () {
   const loader = this.document.querySelector(".loader");
   loader.classList.add("loader-hidden");
 });
+
+class Countries {
+  constructor() {
+    this.countries = [];
+  }
+  addCountries(country) {
+    this.countries.push(country);
+  }
+  setCountries(countries) {
+    this.countries = countries;
+  }
+
+  getCountries() {
+    return this.countries;
+  }
+
+  getCountriesByAmount(input, amount = 24) {
+    const b = input * amount;
+    const a = b - amount;
+    const customArr = this.countries.slice(a, b);
+    return customArr;
+  }
+
+  sortCountries() {
+    this.countries.sort((a, b) => {
+      return a.name.common.localeCompare(b.name.common);
+    });
+  }
+
+  getCountry(countryName) {
+    return this.countries.find(
+      (country) => country.name.common === countryName
+    );
+  }
+}
